65458298534d4f9b9c9f2b9bf6d7c71a5a9cc781
Pedro Cursino <pedrocursino77@gmail.com>
2026-06-30 17:50:15 -0300

    chore: add ai support service project

---
+++ ai-support-service/src/main/java/br/ufrn/smartdesk/aisupport/AiSupportServiceApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.aisupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSupportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSupportServiceApplication.class, args);
+	}
+
+}
---
+++ ai-support-service/src/test/java/br/ufrn/smartdesk/aisupport/AiSupportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.aisupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSupportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
