23b7a98fc1ed381293464ffb1df7ced303abadb2
Pedro Cursino <pedrocursino77@gmail.com>
2026-06-30 17:45:50 -0300

    chore: add ticket orchestrator service project

---
+++ ticket-orchestrator-service/src/main/java/br/ufrn/smartdesk/ticketorchestrator/TicketOrchestratorServiceApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.ticketorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOrchestratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketOrchestratorServiceApplication.class, args);
+	}
+
+}
---
+++ ticket-orchestrator-service/src/test/java/br/ufrn/smartdesk/ticketorchestrator/TicketOrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.ticketorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketOrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
