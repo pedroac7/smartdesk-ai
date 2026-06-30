ad6e92fea1b6f042419bfa9db1735f7388fd6887
Pedro Cursino <pedrocursino77@gmail.com>
2026-06-30 17:53:37 -0300

    chore: add support rules mcp server project

---
+++ support-rules-mcp-server/src/main/java/br/ufrn/smartdesk/supportrulesmcp/SupportRulesMcpServerApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.supportrulesmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportRulesMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportRulesMcpServerApplication.class, args);
+	}
+
+}
---
+++ support-rules-mcp-server/src/test/java/br/ufrn/smartdesk/supportrulesmcp/SupportRulesMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.supportrulesmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportRulesMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
