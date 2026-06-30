a56d14edb261b325aa4f9762fce8a3bad774aa18
Pedro Cursino <pedrocursino77@gmail.com>
2026-06-30 17:52:14 -0300

    chore: add sla function service project

---
+++ sla-function-service/src/main/java/br/ufrn/smartdesk/slafunction/SlaFunctionServiceApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.slafunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaFunctionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlaFunctionServiceApplication.class, args);
+	}
+
+}
---
+++ sla-function-service/src/test/java/br/ufrn/smartdesk/slafunction/SlaFunctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.smartdesk.slafunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaFunctionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
